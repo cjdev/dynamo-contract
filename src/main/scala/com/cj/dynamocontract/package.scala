efe6ce7816a88399d478bc0d53fc1f965021a50b
Daniel Brice <dbrice@cj.com>
2017-06-21 21:13:46 -0700

    Backwards-compatible Feature Addition:
    - Legacy API deprecated
    - API re-arranged around TableConfig
    - Create table via TableConfig
    - Specify region via TableConfig
    - Specify credentials via TableConfig
    - Specify retry behavior via TableConfig via ClientConfig

--- src/main/scala/com/cj/dynamocontract/package.scala
+++ src/main/scala/com/cj/dynamocontract/package.scala
@@ -1,17 +1,31 @@
 package com.cj
 
-
-import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials}
-import com.amazonaws.regions.Regions
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
+import com.amazonaws.{AmazonClientException, AmazonWebServiceRequest}
 import com.amazonaws.services.dynamodbv2.document._
 import com.amazonaws.services.dynamodbv2.document.spec._
-import com.amazonaws.services.dynamodbv2.model.{DeleteTableResult, TableDescription}
+import com.amazonaws.services.dynamodbv2.model._
 
 package object dynamocontract {
-  trait DynamoDBTableContract{
-    def delete():DeleteTableResult
-    def deleteItem(spec: DeleteItemSpec):DeleteItemOutcome
+
+  @deprecated("Replace with DynamoDBTableContract.fromSDKTable")
+  def dynamoTable(table: Table): DynamoDBTableContract =
+    DynamoDBTableContract.fromSDKTable(table)
+
+  @deprecated("Replace with TableConfig.toTable")
+  def getDynamoTable(tableName: String): DynamoDBTableContract =
+    TableConfig(tableName).toTable
+
+  @deprecated("Replace with TableConfig.toTable")
+  def getDynamoTable(
+                      accessKeyId: String,
+                      secretKey: String,
+                      tableName: String
+                    ): DynamoDBTableContract =
+    TableConfig(tableName).withCredentials(accessKeyId, secretKey).toTable
+
+  trait DynamoDBTableContract {
+    def delete(): DeleteTableResult
+    def deleteItem(spec: DeleteItemSpec): DeleteItemOutcome
     def describe(): TableDescription
     def getDescription: TableDescription
     def getIndex(indexName: String): Index
@@ -19,53 +33,170 @@ package object dynamocontract {
     def getItemOutcome(spec: GetItemSpec): GetItemOutcome
     def getTableName: String
     def putItem(item: Item): PutItemOutcome
-    def query(spec: QuerySpec):  ItemCollection[QueryOutcome]
-    def scan(spec: ScanSpec):  ItemCollection[ScanOutcome]
-    def toString:String
+    def query(spec: QuerySpec): ItemCollection[QueryOutcome]
+    def scan(spec: ScanSpec): ItemCollection[ScanOutcome]
     def updateItem(spec: UpdateItemSpec): UpdateItemOutcome
     def updateTable(spec: UpdateTableSpec): TableDescription
     def waitForActive(): TableDescription
     def waitForActiveOrDelete(): TableDescription
     def waitForAllActiveOrDelete(): TableDescription
     def waitForDelete(): Unit
+    def toString: String
   }
 
-  def dynamoTable(table:Table):DynamoDBTableContract = {
-    new  DynamoDBTableContract(){
-      override def delete(): DeleteTableResult                          = table.delete
-      override def deleteItem(spec: DeleteItemSpec): DeleteItemOutcome  = table.deleteItem(spec)
-      override def describe(): TableDescription                         = table.describe
-      override def getDescription: TableDescription                     = table.getDescription
-      override def getIndex(indexName: String): Index                   = table.getIndex(indexName)
-      override def getItem(spec: GetItemSpec): Item                     = table.getItem(spec)
-      override def getItemOutcome(spec: GetItemSpec): GetItemOutcome    = table.getItemOutcome()
-      override def getTableName: String                                 = table.getTableName
-      override def putItem(item: Item): PutItemOutcome                  = table.putItem(item)
-      override def query(spec: QuerySpec): ItemCollection[QueryOutcome] = table.query(spec)
-      override def scan(spec: ScanSpec): ItemCollection[ScanOutcome]    = table.scan(spec)
-      override def toString: String                                     = table.toString
-      override def updateItem(spec: UpdateItemSpec): UpdateItemOutcome  = table.updateItem(spec)
-      override def updateTable(spec: UpdateTableSpec): TableDescription = table.updateTable(spec)
-      override def waitForActive(): TableDescription                    = table.waitForActive
-      override def waitForActiveOrDelete(): TableDescription            = table.waitForActiveOrDelete
-      override def waitForAllActiveOrDelete(): TableDescription         = table.waitForAllActiveOrDelete
-      override def waitForDelete(): Unit                                = table.waitForDelete()
+  object DynamoDBTableContract {
+
+    def fromSDKTable(table: Table): DynamoDBTableContract =
+      new DynamoDBTableContract {
+        def delete(): DeleteTableResult                          = table.delete
+        def deleteItem(spec: DeleteItemSpec): DeleteItemOutcome  = table.deleteItem(spec)
+        def describe(): TableDescription                         = table.describe
+        def getDescription: TableDescription                     = table.getDescription
+        def getIndex(indexName: String): Index                   = table.getIndex(indexName)
+        def getItem(spec: GetItemSpec): Item                     = table.getItem(spec)
+        def getItemOutcome(spec: GetItemSpec): GetItemOutcome    = table.getItemOutcome()
+        def getTableName: String                                 = table.getTableName
+        def putItem(item: Item): PutItemOutcome                  = table.putItem(item)
+        def query(spec: QuerySpec): ItemCollection[QueryOutcome] = table.query(spec)
+        def scan(spec: ScanSpec): ItemCollection[ScanOutcome]    = table.scan(spec)
+        def updateItem(spec: UpdateItemSpec): UpdateItemOutcome  = table.updateItem(spec)
+        def updateTable(spec: UpdateTableSpec): TableDescription = table.updateTable(spec)
+        def waitForActive(): TableDescription                    = table.waitForActive
+        def waitForActiveOrDelete(): TableDescription            = table.waitForActiveOrDelete
+        def waitForAllActiveOrDelete(): TableDescription         = table.waitForAllActiveOrDelete
+        def waitForDelete(): Unit                                = table.waitForDelete()
+        override def toString: String                            = table.toString
+      }
+
+    def fromTableConfig(tableConfig: TableConfig): DynamoDBTableContract =
+      tableConfig.toTable
+  }
+
+  trait ClientConfig {
+
+    def retryDelay(
+                    originalRequest: AmazonWebServiceRequest,
+                    exception: AmazonClientException,
+                    retriesAttempted: Int
+                  ): Option[Long]
+
+    def maxErrorRetry: Int
+  }
+
+  object ClientConfig {
+
+    def exponentialBackoff(
+                            baseDelay: Int = 500,
+                            maxDelay: Long = 20000,
+                            throttledDelay: Int = 25,
+                            throttledMax: Long = 20000,
+                            maxRetry: Int = 10
+                          ): ClientConfig = {
+
+      def expt(base: Long, max: Long): Int => Long =
+        n => math.min((1 to n).map(_ => base).product, max)
+
+      new ClientConfig {
+
+        def retryDelay(
+                        originalRequest: AmazonWebServiceRequest,
+                        exception: AmazonClientException,
+                        retriesAttempted: Int
+                      ): Option[Long] = exception match {
+
+          case e: ItemCollectionSizeLimitExceededException =>
+            Some(expt(baseDelay, maxDelay)(retriesAttempted))
+
+          case e: LimitExceededException =>
+            Some(expt(baseDelay, maxDelay)(retriesAttempted))
+
+          case e: ProvisionedThroughputExceededException =>
+            Some(expt(throttledDelay, throttledMax)(retriesAttempted))
+
+          case _ => None
+        }
+
+        def maxErrorRetry: Int = maxRetry
+      }
     }
   }
 
-  def getDynamoTable(tableName: String): DynamoDBTableContract = dynamoTable(new DynamoDB(Regions.US_WEST_1).getTable(tableName))
+  case class TableConfig(
+                          tableName: String,
+                          regionName: Option[String] = None,
+                          credentials: Option[(String, String)] = None,
+                          clientConfig: Option[ClientConfig] = None
+                        ) {
 
-  def getDynamoTable(
-                      accessKeyId: String,
-                      secretKey: String,
-                      tableName: String
-                    ): DynamoDBTableContract = {
-    val credentials = new BasicAWSCredentials(accessKeyId, secretKey)
-    val provider = new AWSStaticCredentialsProvider(credentials)
-    val client = AmazonDynamoDBClientBuilder.standard()
-      .withCredentials(provider)
-      .withRegion(Regions.US_WEST_1)
-      .build()
-    dynamoTable(new DynamoDB(client).getTable(tableName))
+    def withRegionName(region: String): TableConfig =
+      this.copy(regionName = Some(region))
+
+    def withCredentials(accessKeyId: String, secretKey: String): TableConfig =
+      this.copy(credentials = Some((accessKeyId, secretKey)))
+
+    def withClientConfig(cfg: ClientConfig): TableConfig =
+      this.copy(clientConfig = Some(cfg))
+
+    def toTable: DynamoDBTableContract = {
+
+      import com.amazonaws.ClientConfiguration
+      import com.amazonaws.auth.{AWSCredentials, AWSCredentialsProvider}
+      import com.amazonaws.regions.Regions
+      import com.amazonaws.retry.RetryPolicy
+      import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
+
+      val builder = AmazonDynamoDBClientBuilder.standard()
+
+      builder.setRegion(regionName.getOrElse(Regions.US_WEST_1.getName))
+
+      credentials.foreach { creds =>
+
+        val (aKey, sKey) = creds
+
+        val awsCredentials = new AWSCredentialsProvider {
+          def refresh(): Unit = ()
+          def getCredentials: AWSCredentials = new AWSCredentials {
+            def getAWSAccessKeyId: String = aKey
+            def getAWSSecretKey: String = sKey
+          }
+        }
+
+        builder.setCredentials(awsCredentials)
+      }
+
+      clientConfig.foreach { cfg =>
+
+        val (delay, maxRetries) = (cfg.retryDelay(_, _, _), cfg.maxErrorRetry)
+
+        val retryCondition = new RetryPolicy.RetryCondition {
+          def shouldRetry(
+                           originalRequest: AmazonWebServiceRequest,
+                           exception: AmazonClientException,
+                           retriesAttempted: Int
+                         ): Boolean =
+            delay(originalRequest, exception, retriesAttempted).isDefined
+        }
+
+        val backoffStrategy = new RetryPolicy.BackoffStrategy {
+          def delayBeforeNextRetry(
+                                    originalRequest: AmazonWebServiceRequest,
+                                    exception: AmazonClientException,
+                                    retriesAttempted: Int
+                                  ): Long =
+            delay(originalRequest, exception, retriesAttempted).getOrElse(0)
+        }
+
+        val retryPolicy = new RetryPolicy(retryCondition, backoffStrategy, maxRetries, true)
+
+        val awsConf = new ClientConfiguration()
+          .withRetryPolicy(retryPolicy)
+          .withMaxErrorRetry(maxRetries)
+
+        builder.setClientConfiguration(awsConf)
+      }
+
+      val table = new DynamoDB(builder.build()).getTable(tableName)
+      DynamoDBTableContract.fromSDKTable(table)
+    }
   }
 }
--- src/main/scala/com/cj/dynamocontract/test/DynamoDBTableStub.scala
+++ src/main/scala/com/cj/dynamocontract/test/DynamoDBTableStub.scala
@@ -5,22 +5,22 @@ import com.amazonaws.services.dynamodbv2.document.spec._
 import com.amazonaws.services.dynamodbv2.model.{DeleteTableResult, TableDescription}
 import com.cj.dynamocontract.DynamoDBTableContract
 
-class DynamoDBTableStub extends DynamoDBTableContract {
-  override def delete(): DeleteTableResult = ???
-  override def deleteItem(spec: DeleteItemSpec): DeleteItemOutcome = ???
-  override def describe(): TableDescription = ???
-  override def getDescription: TableDescription = ???
-  override def getIndex(indexName: String): Index = ???
-  override def getItem(spec: GetItemSpec): Item = ???
-  override def getItemOutcome(spec: GetItemSpec): GetItemOutcome = ???
-  override def getTableName: String = ???
-  override def putItem(item: Item): PutItemOutcome = ???
-  override def query(spec: QuerySpec): ItemCollection[QueryOutcome] = ???
-  override def scan(spec: ScanSpec): ItemCollection[ScanOutcome] = ???
-  override def updateItem(spec: UpdateItemSpec): UpdateItemOutcome = ???
-  override def updateTable(spec: UpdateTableSpec): TableDescription = ???
-  override def waitForActive(): TableDescription = ???
-  override def waitForActiveOrDelete(): TableDescription = ???
-  override def waitForAllActiveOrDelete(): TableDescription = ???
-  override def waitForDelete(): Unit = ???
+trait DynamoDBTableStub extends DynamoDBTableContract {
+  def delete(): DeleteTableResult = ???
+  def deleteItem(spec: DeleteItemSpec): DeleteItemOutcome = ???
+  def describe(): TableDescription = ???
+  def getDescription: TableDescription = ???
+  def getIndex(indexName: String): Index = ???
+  def getItem(spec: GetItemSpec): Item = ???
+  def getItemOutcome(spec: GetItemSpec): GetItemOutcome = ???
+  def getTableName: String = ???
+  def putItem(item: Item): PutItemOutcome = ???
+  def query(spec: QuerySpec): ItemCollection[QueryOutcome] = ???
+  def scan(spec: ScanSpec): ItemCollection[ScanOutcome] = ???
+  def updateItem(spec: UpdateItemSpec): UpdateItemOutcome = ???
+  def updateTable(spec: UpdateTableSpec): TableDescription = ???
+  def waitForActive(): TableDescription = ???
+  def waitForActiveOrDelete(): TableDescription = ???
+  def waitForAllActiveOrDelete(): TableDescription = ???
+  def waitForDelete(): Unit = ???
 }
